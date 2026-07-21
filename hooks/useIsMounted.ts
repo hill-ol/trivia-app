8b8fd237003dcefdde16b2ec90ece7fcdce11a4a
Olivia Hill <hill.ol@northeastern.edu>
2026-07-21 17:34:23 -0400

    feat: guard async handlers against state updates after unmount

---
+++ hooks/useIsMounted.ts
@@ -0,0 +1,16 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+
+export function useIsMounted() {
+    const isMountedRef = useRef(true)
+
+    useEffect(() => {
+        isMountedRef.current = true
+        return () => {
+            isMountedRef.current = false
+        }
+    }, [])
+
+    return () => isMountedRef.current
+}
